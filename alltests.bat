8ec4e4a40d18ed95211e701de79d36e082874eba
Tony Wilson <thinqa@myopera.com>
2013-10-27 11:56:16 +0200

    disambiguate filenames; fix alltests.bat

--- alltests.bat
+++ alltests.bat
@@ -1,2 +1,2 @@
-@go test -i . ./atk ./cairo ./expat ./fontconfig ./freetype2 ./gail ./gdk ./gio ./glib ./gmodule ./gobject ./gtk ./gtksourceview ./intl ./pango ./pangowin32 ./png ./types ./zlib
-@go test . ./atk ./cairo ./expat ./fontconfig ./freetype2 ./gail ./gdk ./gio ./glib ./gmodule ./gobject ./gtk ./gtksourceview ./intl ./pango ./pangowin32 ./png ./types ./zlib
+@go test -i . ./atk ./cairo ./expat ./fontconfig ./freetype2 ./gail ./gdk ./gio ./glib ./gmodule ./gobject ./gtk ./gtksourceview ./intl ./pango ./png ./types ./zlib
+@go test . ./atk ./cairo ./expat ./fontconfig ./freetype2 ./gail ./gdk ./gio ./glib ./gmodule ./gobject ./gtk ./gtksourceview ./intl ./pango ./png ./types ./zlib
